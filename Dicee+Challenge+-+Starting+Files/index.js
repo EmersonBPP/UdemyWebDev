207fd15c884bce7ab38a6e59a7f5fef2b3190d33
Emerson Pedro <emersonborgespintopedr@gmail.com>
2025-09-17 11:29:47 +0100

    Dice Challenge
    Used the function to remove the blink efect when refresh the page

---
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,23 @@
+function rollDices() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+  }
+  else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+  }
+  else {
+    document.querySelector("h1").textContent = "Draw!";
+  }
+}
+
+// Run once on page load
+rollDices();
+
+// Run again when button is clicked
+document.querySelector("button").addEventListener("click", rollDice);
